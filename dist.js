ce1ff2782c77ea981db6aeef9b6265df00e91c3e
Nils Dijk <nils@uber.com>
2016-10-31 14:20:37 +0100

    add functionality to provide hashfunction and keys to ringpop-admin dist and view its distribution.

--- dist.js
+++ dist.js
@@ -26,11 +26,16 @@ var AdminClient = require('./lib/admin-client.js');
 var createTable = require('./lib/table.js').create;
 var HashRing = require('ringpop/lib/ring/index.js');
 var program = require('commander');
+var fs = require('fs');
+var farmhash = require('farmhash');
 
 function main() {
     program
         .description('Distribution of keyspace')
         .option('--tchannel-v1')
+        .option('--sevnup <vnode>', 'numer of vnodes used by sevnup', 0, parseInt)
+        .option('--file <filename>', 'name of file to read for keys to calculate distribution for. It will be tried to parse as a JSON array, if it fails it will use newline seperated fileformat.')
+        .option('--hash <hashfunction>', 'hash function to use for the hashring, defaults to farmhash.fingerprint32', 'fingerprint32')
         .usage('[options] <hostport>')
         .parse(process.argv);
 
@@ -41,10 +46,18 @@ function main() {
         process.exit(1);
     }
 
-    var table = createTable([
-        'address',
-        'percentage'
-    ]);
+    var hasher;
+    switch(program.hash) {
+        case 'fingerprint32':
+            hasher = farmhash.fingerprint32;
+            break;
+        case 'hash32':
+            hasher = farmhash.hash32v1;
+            break;
+        default:
+            console.error('unknown hashfunction:', program.hash);
+            process.exit(1);
+    }
 
     var node = new AdminClient({
         useTChannelV1: program.tchannelV1
@@ -62,7 +75,9 @@ function main() {
             }
         });
 
-        var hashRing = new HashRing();
+        var hashRing = new HashRing({
+            hashFunc: hasher
+        });
 
         for (var i = 0; i < members.length; i++) {
             var member = members[i];
@@ -95,6 +110,41 @@ function main() {
         var minOwnage = 101;
         var minOwners = [];
 
+        var maxOwnageKeys;
+        var maxOwnersKeys = [];
+        var minOwnageKeys;
+        var minOwnersKeys = [];
+
+        var keys = [];
+        if (program.sevnup) {
+            // add keys for sevnup
+            for (var j = 0; j<program.sevnup; j++) {
+                keys.push('' + j);
+            }
+        }
+
+        if (program.file) {
+            var data = fs.readFileSync(program.file);
+
+            try {
+                data = JSON.parse(data);
+            } catch (e) {
+                data = data.toString().trim().split('\n');
+            }
+            data.forEach(function addKeysFromFile(key) {
+                keys.push(key);
+            });
+        }
+
+        var keysPerMember = {};
+        keys.forEach(function (key) {
+            var member = hashRing.lookup(key);
+            if (!keysPerMember[member]) {
+                keysPerMember[member] = 0;
+            }
+            keysPerMember[member]++;
+        });
+
         var rows = [];
 
         members.forEach(function eachMember(member) {
@@ -118,11 +168,38 @@ function main() {
                 minOwnage = percentageOwned;
             }
 
+            var keysOwned = keysPerMember[member.address] || 0;
+            var percentageOwnedKeys = keysOwned / keys.length * 100;
+
+            if (maxOwnageKeys === undefined || percentageOwnedKeys > maxOwnageKeys) {
+                maxOwnageKeys = percentageOwnedKeys;
+                maxOwnersKeys = [];
+                maxOwnersKeys.push(member.address);
+            }
+
+            if (minOwnageKeys === undefined || percentageOwnedKeys < minOwnageKeys) {
+                minOwnageKeys = percentageOwnedKeys;
+                minOwnersKeys = [];
+                minOwnersKeys.push(member.address);
+            }
+
             rows.push({
                 address: member.address,
-                percentage: percentageOwned
+                percentage: percentageOwned,
+                keysOwned: keysOwned,
+                keyPercentage: percentageOwnedKeys
             });
         });
+
+        var headers = [
+            'address',
+            'percentage'
+        ];
+        if (keys.length) {
+            headers.push('keys');
+            headers.push('keys percentage');
+        }
+        var table = createTable(headers);
 
         rows.forEach(function eachRow(row) {
             var addressVal = row.address;
@@ -140,10 +217,27 @@ function main() {
                 percentageVal = formatPercent(row.percentage);
             }
 
-            table.push([
+            var values = [
                 addressVal,
                 percentageVal
-            ]);
+            ];
+
+            if (keys.length) {
+                var color;
+                if (row.keyPercentage === maxOwnageKeys) {
+                    color = CliColor.green;
+                } else if (row.keyPercentage === minOwnageKeys) {
+                    color = CliColor.yellow;
+                } else {
+                    color = function noColor(s) {
+                        return s;
+                    };
+                }
+                values.push(color(row.keysOwned));
+                values.push(color(formatPercent(row.keyPercentage)));
+            }
+
+            table.push(values);
         });
 
         console.log(table.toString());
